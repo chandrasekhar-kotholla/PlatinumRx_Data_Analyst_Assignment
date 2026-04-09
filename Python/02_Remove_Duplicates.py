78d4cea463e52329de9bfeba91f947184d513f62
chandrasekhar-kotholla <chandrachandhu2004@gmail.com>
2026-04-09 15:14:27 +0530

    Update 02_Remove_Duplicates.py

--- Python/02_Remove_Duplicates.py
+++ Python/02_Remove_Duplicates.py
@@ -1,1 +1,30 @@
+def remove_duplicates(s):
+    result = ""
+    
+    for char in s:
+        if char not in result:
+            result += char
+    
+    return result
+
+n = int(input())  # number of test cases
+
+for _ in range(n):
+    string = input()
+    print(remove_duplicates(string)) 
+
+
+## INPUT:
+3
+aabbcc
+programming
+hello world
+
+
+## OUTPUT:
+abc
+progamin
+helo wrd
+
+
 
